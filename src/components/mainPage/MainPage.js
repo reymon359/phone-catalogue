d0974dbbad46f85109fbb7c60a7a82b12895d30e
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-01 16:57:19 +0100

    Create Header component & update Snapshots

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import Header from '../header/Header';
+
 export class MainPage extends Component {
     componentDidMount() {
         this.props.onRequestPhones();
     }
 
     render() {
-        return <div className="tc">hi</div>;
+        return (
+            <div className="tc">
+                <Header />
+            </div>
+        );
     }
 }
 
