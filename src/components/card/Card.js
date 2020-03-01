f17f0d8942efa60d3301a6200af9d42d745f00c3
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-01 17:12:05 +0100

    Refactor Card and CardList

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const Card = ({ id, name, price, imageFileName }) => {
+const Card = (props) => {
+    const {phone} = props;
     return (
         <div className="bg-white black dib br3 ma2 grow bw2 shadow-2-l pointer">
             <img
-                src={imageFileName}
+                src={phone.imageFileName}
                 className="br3"
                 width="auto"
                 height="200"
                 alt="phones"
             />
             <div>
-                <h2>{name}</h2>
-                <p className="b">{price} 💲</p>
+                <h2>{phone.name}</h2>
+                <p className="b">
+                    {phone.price}
+                    <span role="img" aria-label="money-emoji">💲</span>
+                </p>
             </div>
         </div>
     );
--- src/components/cardList/CardList.js
+++ src/components/cardList/CardList.js
@@ -4,17 +4,9 @@ import Card from '../card/Card';
 const CardList = ({ phones }) => {
     return (
         <div>
-            {phones.map((phone, i) => {
-                return (
-                    <Card
-                        key={i}
-                        id={phones[i].id}
-                        name={phones[i].name}
-                        price={phones[i].price}
-                        imageFileName={phones[i].imageFileName}
-                    />
-                );
-            })}
+            {phones.map((phone, i) => (
+                <Card phone={phone} key={i}/>
+            ))}
         </div>
     );
 };
